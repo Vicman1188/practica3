131ca8ff0d13a59fb5afd0d32110c635e11f5483
vicmangoes <geim_313@hotmail.com>
2015-03-13 13:58:40 +0000

    first commit estructura basica de slim

--- public/index.php
+++ public/index.php
@@ -1,10 +1,23 @@
 <?php
 	require("../vendor/autoload.php");
 
-	$app = new \Slim\Slim();
+	$app = new \Slim\Slim(
+	 array(
+		'view' => new \Slim\Views\Blade(),
+		'templates.path' => '../templates',
+		)
+		);
+		
+		$view = $app->view();
+	$view->parserOptions = [
+		'debug' => true,
+		'cache' => "../html_cache"
+	];
+	
 
-	$app->get('/hey', function(){
-		echo "hi there!!";
+	$app->get('/hey', function() use ($app){
+		
+		$app->render('index');
 	});
 
 	$app->run();
